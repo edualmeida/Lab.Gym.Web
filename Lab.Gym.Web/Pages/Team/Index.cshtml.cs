a2f316d35e8746bf1f06fa79ac4e43873366e4f7
Eduardo Almeida <edu.almeida1@gmail.com>
2024-01-16 17:04:26 +0000

    Web site new design changes

---
+++ Lab.Gym.Web/Pages/Team/Index.cshtml.cs
@@ -0,0 +1,15 @@
+using Lab.Gym.Web.Pages.Shared.Components;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab.Gym.Web.Pages.Team
+{
+    public class IndexModel(ILogger<IndexModel> logger) : UserPageModel
+    {
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public void OnGet()
+        {
+        }
+    }
+}
